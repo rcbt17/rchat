2a59760c75755d97245bd627384327cc5a474c92
Robin Ciubotaru <robinciubotaru17@gmail.com>
2025-09-01 12:05:08 +0300

    Creation and listing of chatrooms

---
+++ app/controllers/chatrooms_controller.rb
@@ -0,0 +1,30 @@
+class ChatroomsController < ApplicationController
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.new(chatroom_params)
+    @chatroom.user = Current.user
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom), notice: "Congrats for your new chatroom!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def index
+    @chatrooms = Chatroom.all.order(created_at: :desc)
+    @user_chatrooms = Chatroom.where(user: Current.user)
+  end
+
+  private
+
+  def chatroom_params
+   params.require(:chatroom).permit(:name)
+  end
+end
